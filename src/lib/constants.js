d4087fb23c8afeab1eaa76effd290f782598aab7
Bodichka <cln.brtnlv@gmail.com>
2026-05-07 10:33:06 +0300

    Update user constants with new user data

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,8 +1,8 @@
 // Фіксовані користувачі (логін / пароль / колір)
 export const USERS = [
-  { id: 'user1', name: 'Аліна',   email: 'alina@wayplanner.app',   password: 'alina2024',   color: '#e8622a', avatar: '🧡' },
-  { id: 'user2', name: 'Богдан',  email: 'bohdan@wayplanner.app',  password: 'bohdan2024',  color: '#2a7de8', avatar: '💙' },
-  { id: 'user3', name: 'Вікторія',email: 'viktoria@wayplanner.app',password: 'viktoria2024',color: '#2abf6e', avatar: '💚' },
+  { id: 'user1', name: 'Марія',   email: 'utkinam@wayplanner.app',   password: 'maria2024',   color: '#e8622a', avatar: '🧡' },
+  { id: 'user2', name: 'Богдан',  email: 'pishcholinb@wayplanner.app',  password: 'bohdan2024',  color: '#2a7de8', avatar: '💙' },
+  { id: 'user3', name: 'Вікторія',email: 'kuschn@wayplanner.app',password: 'nikita2024',color: '#2abf6e', avatar: '💚' },
 ]
 
 export const POINT_TYPES = {
