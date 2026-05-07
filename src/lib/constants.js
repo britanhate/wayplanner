ecbc52beb2a041df916e4310b48f16d9a63e0ccb
Bodichka <cln.brtnlv@gmail.com>
2026-05-07 10:35:15 +0300

    Update user name from 'Вікторія' to 'Нікіта'

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,7 +2,7 @@
 export const USERS = [
   { id: 'user1', name: 'Марія',   email: 'utkinam@wayplanner.app',   password: 'maria2024',   color: '#e8622a', avatar: '🧡' },
   { id: 'user2', name: 'Богдан',  email: 'pishcholinb@wayplanner.app',  password: 'bohdan2024',  color: '#2a7de8', avatar: '💙' },
-  { id: 'user3', name: 'Вікторія',email: 'kuschn@wayplanner.app',password: 'nikita2024',color: '#2abf6e', avatar: '💚' },
+  { id: 'user3', name: 'Нікіта',email: 'kuschn@wayplanner.app',password: 'nikita2024',color: '#2abf6e', avatar: '💚' },
 ]
 
 export const POINT_TYPES = {
