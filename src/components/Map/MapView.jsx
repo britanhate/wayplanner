200ec5712cd8345224a2c67b823d3bd11d2483a0
britanhate <cln.brtnlv@gmail.com>
2026-05-08 09:27:32 +0300

    fix: Change zoom control position from top right to bottom right

--- src/components/Map/MapView.jsx
+++ src/components/Map/MapView.jsx
@@ -46,7 +46,7 @@ export default function MapView({ sidebarOpen, onSidebarClose }) {
 
     L.control
       .zoom({
-        position: "topright",
+        position: "bottomright",
       })
       .addTo(mapInstance.current);
 
