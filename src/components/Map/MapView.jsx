7bd73dd6b0c03fe7cffa3315f7d1bf4948b1d393
britanhate <cln.brtnlv@gmail.com>
2026-05-10 14:34:07 +0300

    change map to topo

--- src/components/Map/MapView.jsx
+++ src/components/Map/MapView.jsx
@@ -40,8 +40,8 @@ export default function MapView({ sidebarOpen, onSidebarClose }) {
       zoomControl: false,
     }).setView([48.8566, 2.3522], 12);
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-      attribution: "© OpenStreetMap © CARTO",
+    L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap © OpenTopoMap",
       maxZoom: 19,
     }).addTo(mapInstance.current);
 
