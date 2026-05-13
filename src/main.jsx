81010ac9acd5dc9f29514c91bce787504348eddf
britanhate <cln.brtnlv@gmail.com>
2026-05-13 12:55:55 +0300

    feat(performance): add @vercel/speed-insights for enhanced performance tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import { AuthProvider, useAuth } from "./lib/AuthContext";
 import App from "./pages/App";
 import Login from "./pages/Login";
@@ -20,5 +22,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </AuthProvider>
 
     <Analytics />
+    <SpeedInsights />
   </>,
 );
